9d26311438a84af8672daaa9ed860f3f4459b981
Paulo <pauloantoniolfc@gmail.com>
2019-11-27 08:07:26 -0300

    inicio do map

--- src/pages/Mapa/index.js
+++ src/pages/Mapa/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGF1bG9hbnRvbmlvbCIsImEiOiJjazNnbjc4bXgwMzBrM29xbG5mZ282MHVuIn0.VRooPx9e-gEkUEQLokrudg';
 
 export default function Mapa() {
-  return <>Mapa</>;
+  return (
+    <>
+      <div>Paulinho</div>
+    </>
+  );
 }
